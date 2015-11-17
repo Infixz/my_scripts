d870e251c19db717bc0bb9489115ac7a10c50152
jonathan <infixz520@gmail.com>
2015-11-17 19:24:27 +0800

    send msg instant

--- socket_client.py
+++ socket_client.py
@@ -2,15 +2,17 @@
 # coding:utf-8
 
 import socket
+import time
 
-HOST = "localhost"
+HOST = "42.96.204.277"
 PORT = 25000
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 
-s.send('hehe,is it works well ?')
-received_data = s.recv(4096)
-s.close()
-
-print 'received from socket server',received_data
+while 1:
+	time.sleep(2)
+	s.send('hehe,is it works well ?')
+	received_data = s.recv(4096)
+	s.close()
+	print 'received from socket server',received_data
