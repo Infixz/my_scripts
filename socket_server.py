9aa953dda11fb94e07f58b92cbabc2ac482414c7
jonathan <infixz520@gmail.com>
2015-11-17 19:33:34 +0800

    sever listen

--- socket_server.py
+++ socket_server.py
@@ -8,13 +8,13 @@ PORT = 25000
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((HOST,PORT))
-s.listen(1)
+s.listen(3)
 conn,addr = s.accept()
 
 print 'Connect by ',addr
 while 1:
 	data = conn.recv(4096)
-	print '\n recv = ',dataS
+	print '\n recv = ',data
 	if not data:break
 	conn.sendall(data.upper())
 conn.close()
